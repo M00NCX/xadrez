364bca73f21bb3ba07a9284fdbf35ad3a3888898
Hiago Moura Carvalho <84892686+HiagoMCarvalho@users.noreply.github.com>
2022-10-27 23:42:15 -0300

    Update XadrezMod.java
    Protótipo de main v2

--- src/xadrezmod/XadrezMod.java
+++ src/xadrezmod/XadrezMod.java
@@ -4,50 +4,53 @@ import java.util.Scanner;
 import xadrezmod.PIECES.Color;
 import xadrezmod.PIECES.Torre;
 import xadrezmod.TABULEIRO.Tabuleiro;
-public class XadrezMod {
-
-    public static void main(String[] args) {
-   
+public class XadrezMod 
+{
+        public static void main(String[] args) 
+    {
         int cfX;
         int cfY;
+        Peca torre = new Torre();
+        torre.setColor('w');
         
-        Color cor = null;
-        //CRIANDO AS PEÇAS
-        Torre torreW = new Torre(7, 0, cor.WHITE);
-        Torre torreW1 = new Torre(7, 7, cor.WHITE);
-        Torre torreB = new Torre(0, 0, cor.BLACK);
-        Torre torreB1 = new Torre(0, 7, cor.BLACK);
-        
+        Peca sp = new Torre();
+        sp.setColor('b');
+       
         Tabuleiro tab = new Tabuleiro();
+        
         Scanner in = new Scanner(System.in);
         Scanner aft = new Scanner(System.in);
      
         System.out.println("Onde esta cordenada X da torre?: ");
-        torreW.setPogX(in.nextInt());
+        torre.setPogX(in.nextInt());
     
         System.out.println("Onde esta a cordenada Y da torre?: ");
-        torreW.setPogY(in.nextInt());
+        torre.setPogY(in.nextInt());
+        
+        tab.setEnd(torre, true);
     
         System.out.println("Para onde vai a cordenada X da torre");
         cfX = aft.nextInt();
     
         System.out.println("Para onde vai a cordenada Y da torre");
         cfY = aft.nextInt();
-    
-       if(tab.checkEnderecos(torreW.getPogX(), torreW.getPogY()))
-       {
-           if(torreW.movimento(torreW.getPogX(), torreW.getPogY(), cfX, cfY))
-           {
-               torreW.setPogX(cfX);
-               torreW.setPogY(cfY);
-               tab.setEnderecos(false,torreW.getPogX(), torreW.getPogY());
-               tab.setEnderecos(true, cfX, cfY);
-           }
-           
-       }
         
+        sp.setPogX(cfX);
+        sp.setPogY(cfY);
+        tab.setEnd(sp, true);
+        
+       if(torre.movimento(torre.getPogX(), torre.getPogY(), cfX, cfY))
+       {
+           if(tab.checkEnd(torre.getPogX(),torre.getPogY(), cfX, cfY))
+           {
+               tab.setEnd(torre, false);
+               torre.setPogX(cfX);
+               torre.setPogY(cfY);
+               tab.setEnd(torre, true);
+               
+           }
+       }
     }
     
-        
-}
+}    
     
